535926f538a36f56f7b25aa9707afbe45250e951
chuanzong.lcz@alibaba-inc.com <chuanzong.lcz@alibaba-inc.com>
2021-04-06 00:28:04 +0800

    test: 对于数组必须所有元素都满足某个条件

--- test/index.test.js
+++ test/index.test.js
@@ -18,6 +18,7 @@ describe('js2schema', () => {
       "properties": {
         "site": {
           "type": "array",
+          "description": "site",
           "items": {
             "type": "object",
             "properties": {
@@ -30,9 +31,8 @@ describe('js2schema', () => {
                 "description": "https://img.alicdn.com/tfs/TB1qEwuzrj1gK0jSZFOXXc7GpXa-32-32.ico"
               }
             },
-            "description": "site/0"
-          },
-          "description": "site"
+            "description": "items"
+          }
         }
       },
       "description": "configurations"
@@ -69,7 +69,7 @@ describe('js2schema', () => {
               "description": "description of left part"
             },
             "number1": {
-              "type": "number",
+              "type": "integer",
               "description": "30000.00"
             },
             "number2": {
@@ -185,7 +185,7 @@ describe('js2schema', () => {
                   "description": "true"
                 }
               },
-              "description": "middle/nested"
+              "description": "nested"
             }
           },
           "description": "middle"
@@ -195,6 +195,329 @@ describe('js2schema', () => {
     };
 
     // console.log('actual:', JSON.stringify(actual, null, 2));
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('array of objects', () => {
+    const input = [
+      {
+        "link": "wechat://",
+        "seed": "wechat://",
+        "image": "https://t.alipayobjects.com/images/rmsweb/T1xwNjXctcXXXXXXXX.jpeg_640xQ75"
+      }
+    ];
+    const actual = js2schema(input, { title: 'Banner，填单个图片时不轮播' });
+    const expected = {
+      "$schema": "http://json-schema.org/draft-04/schema#",
+      "title": "Banner，填单个图片时不轮播 Set",
+      "type": "array",
+      "items": {
+        "type": "object",
+        "properties": {
+          "link": {
+            "type": "string",
+            "description": "wechat://"
+          },
+          "seed": {
+            "type": "string",
+            "description": "wechat://"
+          },
+          "image": {
+            "type": "string",
+            "description": "https://t.alipayobjects.com/images/rmsweb/T1xwNjXctcXXXXXXXX.jpeg_640xQ75"
+          }
+        },
+        "title": "Banner，填单个图片时不轮播",
+        "description": "Banner，填单个图片时不轮播"
+      },
+      "description": "Banner，填单个图片时不轮播 Set"
+    };
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('Should add all the strings in array as the description', () => {
+    const products = [
+      {
+        tags: ["cold", "ice"],
+      }
+    ];
+    const actual = js2schema(products, { title: 'Product' });
+
+    const expected = {
+      "$schema": "http://json-schema.org/draft-04/schema#",
+      "title": "Product Set",
+      "description": "Product Set",
+      "type": "array",
+      "items": {
+        "type": "object",
+        "title": "Product",
+        "description": "Product",
+        "properties": {
+          "tags": {
+            "type": "array",
+            "description": "cold | ice",
+            "items": {
+              "type": "string",
+              "description": "cold"
+            }
+          }
+        }
+      }
+    };
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('Should support integer type in example of `generate-schema`', () => {
+    const input = [
+      {
+        "id": 2,
+        "name": "An ice sculpture",
+        "price": 12.50,
+        "tags": ["cold", "ice"],
+        "dimensions": {
+          "length": 7.0,
+          "width": 12.0,
+          "height": 9.5
+        },
+        "warehouseLocation": {
+          "latitude": -78.75,
+          "longitude": 20.4
+        }
+      },
+      {
+        "id": 3,
+        "name": "A blue mouse",
+        "price": 25.50,
+        "dimensions": {
+          "length": 3.1,
+          "width": 1.0,
+          "height": 1.0
+        },
+        "warehouseLocation": {
+          "latitude": 54.4,
+          "longitude": -32.7
+        }
+      }
+    ];
+    const actual = js2schema(input, { title: 'Product' });
+    const expected = {
+      "$schema": "http://json-schema.org/draft-04/schema#",
+      "title": "Product Set",
+      "description": "Product Set",
+      "type": "array",
+      "items": {
+        "description": "Product",
+        "title": "Product",
+        "type": "object",
+        "properties": {
+          "id": {
+            "type": "integer",
+            "description": "2",
+          },
+          "name": {
+            "type": "string",
+            "description": "An ice sculpture"
+          },
+          "price": {
+            "type": "number",
+            "description": "12.5"
+          },
+          "tags": {
+            "description": "cold | ice",
+            "type": "array",
+            "items": {
+              "description": "cold",
+              "type": "string"
+            }
+          },
+          "dimensions": {
+            "type": "object",
+            "description": "dimensions",
+            "properties": {
+              "length": {
+                "type": "number",
+                "description": "7"
+              },
+              "width": {
+                "type": "integer",
+                "description": "12"
+              },
+              "height": {
+                "type": "number",
+                "description": "9.5"
+              }
+            }
+          },
+          "warehouseLocation": {
+            "type": "object",
+            "description": "warehouseLocation",
+            "properties": {
+              "latitude": {
+                "type": "number",
+                "description": "-78.75"
+              },
+              "longitude": {
+                "type": "number",
+                "description": "20.4"
+              }
+            }
+          }
+        },
+        "required": [
+          "id",
+          "name",
+          "price",
+          "dimensions",
+          "warehouseLocation"
+        ]
+      }
+    };
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('Should type be integer when all of the values are integer number', () => {
+    const input = [
+      {
+        "id": 2,
+      },
+      {
+        "id": 3,
+      },
+      {
+        "id": 3.00,
+      },
+    ];
+
+    const actual = js2schema(input, { title: 'Product' });
+    const expected = {
+      "$schema": "http://json-schema.org/draft-04/schema#",
+      "title": "Product Set",
+      "description": "Product Set",
+      "type": "array",
+      "items": {
+        "description": "Product",
+        "title": "Product",
+        "type": "object",
+        "properties": {
+          "id": {
+            "type": "integer",
+            "description": "2",
+          },
+        },
+        "required": [
+          "id",
+        ]
+      }
+    };
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('Should type be integer when all of the values are integer string', () => {
+    const input = [
+      {
+        "id": '4',
+      },
+      {
+        "id": '4.00',
+      },
+    ];
+
+    const actual = js2schema(input, { title: 'Product' });
+    const expected = {
+      "$schema": "http://json-schema.org/draft-04/schema#",
+      "title": "Product Set",
+      "description": "Product Set",
+      "type": "array",
+      "items": {
+        "description": "Product",
+        "title": "Product",
+        "type": "object",
+        "properties": {
+          "id": {
+            "type": "integer",
+            "description": "4",
+          },
+        },
+        "required": [
+          "id",
+        ]
+      }
+    };
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('Should type be number when any of the values is float number', () => {
+    const input = [
+      {
+        "id": 2,
+      },
+      {
+        "id": 3.1,
+      },
+    ];
+
+    const actual = js2schema(input, { title: 'Product' });
+    const expected = {
+      "$schema": "http://json-schema.org/draft-04/schema#",
+      "title": "Product Set",
+      "description": "Product Set",
+      "type": "array",
+      "items": {
+        "description": "Product",
+        "title": "Product",
+        "type": "object",
+        "properties": {
+          "id": {
+            "type": "number",
+            "description": "2",
+          },
+        },
+        "required": [
+          "id",
+        ]
+      }
+    };
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('Should type be number when any of the values is float string', () => {
+    const input = [
+      {
+        "id": '2',
+      },
+      {
+        "id": '3.1',
+      },
+    ];
+
+    const actual = js2schema(input, { title: 'Product' });
+    const expected = {
+      "$schema": "http://json-schema.org/draft-04/schema#",
+      "title": "Product Set",
+      "description": "Product Set",
+      "type": "array",
+      "items": {
+        "description": "Product",
+        "title": "Product",
+        "type": "object",
+        "properties": {
+          "id": {
+            "type": "number",
+            "description": "2",
+          },
+        },
+        "required": [
+          "id",
+        ]
+      }
+    };
 
     expect(actual).toEqual(expected);
   });
